c1c39c8010fa16697b6ff824bec17f5faa683438
Aamir khan <101388992+aamirkhan9420@users.noreply.github.com>
2023-02-25 19:55:02 +0530

    Update application.model.js

--- model/application.model.js
+++ model/application.model.js
@@ -2,7 +2,6 @@ const mongoose=require('mongoose')
 const applicationschema=mongoose.Schema({
     name:String,
     email:String,
-    password:String,
     state:String,
     course:String,
     coursetime:String,
